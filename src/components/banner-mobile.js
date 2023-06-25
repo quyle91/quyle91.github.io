1cf196ba1f7276ca3c053495284407ea52bcdff8
quyle <quylv.dsth@gmail.com>
2023-06-25 10:25:23 +0700

    Updates

--- src/components/banner-mobile.js
+++
@@ -1,8 +0,0 @@
-const BannerMobile = () => {
-      return (
-            <>
-            <div id="banner-mobile"className="bgimg w3-grayscale-max w3-hide-large" style={{height: "300px", width: "100%", backgroundImage: "url(./img/quyle.jpg)", backgroundPosition: "center center"}} ></div>
-            </>
-      )
-}
-export default BannerMobile
--- src/components/templates/pdfbutton.js
+++
@@ -1,7 +0,0 @@
-const PDFButton = ({ onClick }) => {
-    return (
-    <button onClick={onClick}>Tạo và tải xuống PDF</button>
-    );
-};
-
-export default PDFButton
